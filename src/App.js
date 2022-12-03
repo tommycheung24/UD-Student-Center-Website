824f292ad45a6b416b72c68dd4783f6750535e37
tcheung <tcheung@udel.edu>
2022-12-02 23:24:48 -0500

    add schedule of perkin and trabant

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './home/home.js';
 import NavigationBar from './navigationBar/navigationBar';
--- src/home/home.js
+++ src/home/home.js
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react';
+import './home.css'
+const perkinsExterior = require('../resources/Perkins-Exterior.png');
+const trabantExterior = require('../resources/Trabant-Exterior.png');
+
+
 export default function Home() {
+    const [status, setStatus] = useState('Open');
+    const [reminder, setReminder] = useState('Reminder');
+    useEffect(()=>{
+        let today = new Date();
+        let schedule = getSchedule(today);
+
+        if(today < schedule['open'] || today >= schedule['close']){
+            let tomorrow = new Date(today);
+            tomorrow.setDate(tomorrow+1);
+            let tomorrowSchedule = getSchedule(tomorrow)
+            setStatus('Close');
+            setReminder('Opens at ' +  tomorrowSchedule['open'].toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })  +  ' tomorrow')
+        }else{
+            setReminder('Closes at ' + schedule['close'].toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }))
+        }
+
+    }, [])
+
     return (
-        <h1>Hello</h1>
+        <div className='buildings'>
+            <div className="perkinsContainer">
+                <text className='title'>Perkins Student Center</text>
+                <a href='https://sites.udel.edu/usc/buildings/perkins/'>
+                    <img src={perkinsExterior} className='perkinsExterior' alt="perkins" />
+                </a>
+                <text className='status'>{status}</text>
+                <text className='reminder'>{reminder}</text>
+            </div>
+            <div className="trabantContainer">
+                <text className='title'>Trabant Student Center</text>
+                <a href='https://sites.udel.edu/usc/buildings/trabant/'>
+                    <img src={trabantExterior} className='trabantExterior' alt="trabant" />
+                </a>
+                <text className='status'>{status}</text>
+                <text className='reminder'>{reminder}</text>
+            </div>
+        </div>
     );
 }
+
+function createTime(day, hours, minutes){
+    return new Date(day.getFullYear(), day.getMonth(), day.getDate(), hours, minutes, 0, 0);
+}
+
+function getSchedule(day){
+    let startHour = 7
+    let endHour = 24
+    if (day.getDate() === 0 || day.getDate() === 6){
+        startHour = 9
+    }
+    return {'open': createTime(day, startHour,0), 'close': createTime(day, endHour,0)}
+}
+
+
